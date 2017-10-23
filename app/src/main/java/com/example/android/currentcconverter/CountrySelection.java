cfdc37cb71a53fa0b66c7b46cbbc8f8d0b8291e6
Nhan Nguyen <nhan.nguyen@uwaterloo.ca>
2017-10-22 23:13:50 -0400

    Add CountrySelection Activity

---
+++ app/src/main/java/com/example/android/currentcconverter/CountrySelection.java
@@ -0,0 +1,13 @@
+package com.example.android.currentcconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CountrySelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_country_selection);
+    }
+}
