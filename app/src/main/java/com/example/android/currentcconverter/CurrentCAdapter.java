38480ac420daba7841893390ee4bfd87b3deeb94
Nhan Nguyen <nhan.nguyen@uwaterloo.ca>
2017-10-26 18:53:16 -0400

    Added ViewHolder to ArrayAdapter to enhance performance

--- app/src/main/java/com/example/android/currentcconverter/CurrentCAdapter.java
+++ app/src/main/java/com/example/android/currentcconverter/CurrentCAdapter.java
@@ -16,6 +16,13 @@ import java.util.ArrayList;
 
 public class CurrentCAdapter extends ArrayAdapter<CurrentC> {
 
+    //Set up a ViewHolder to enhance scrolling performance
+    private static class ViewHolder {
+        TextView nameTextView;
+        TextView abbrTextView;
+        ImageView flagImageView;
+    }
+
     //Set up CurrentCAdapter
     public CurrentCAdapter (Activity context, ArrayList<CurrentC> currentCArrayList) {
         super(context, 0, currentCArrayList);
@@ -25,28 +32,35 @@ public class CurrentCAdapter extends ArrayAdapter<CurrentC> {
     public View getView(int position, View convertView, ViewGroup parent) {
         //Check if the existing view is used, if not produces one
         View listItemView = convertView;
-
+        ViewHolder viewHolder; //Look up cache stored in tag
         if (listItemView == null) {
             //Inflate using list_item.xml as layout
             listItemView = LayoutInflater
                     .from(getContext())
                     .inflate(R.layout.list_item, parent, false);
+            viewHolder = new ViewHolder();
+
+            //Initialize View to set texts and images
+            viewHolder.nameTextView = listItemView.findViewById(R.id.nameTextView);
+            viewHolder.abbrTextView = listItemView.findViewById(R.id.abbrTextView);
+            viewHolder.flagImageView = listItemView.findViewById(R.id.flagImageView);
+            listItemView.setTag(viewHolder);
+        }
+        else {
+            viewHolder = (ViewHolder) listItemView.getTag();
         }
 
         // Get the {@link AndroidFlavor} object located at this position in the list
         CurrentC currentC = getItem(position);
 
         //Set nameTextView in list_item
-        TextView nameTextView = (TextView) listItemView.findViewById(R.id.nameTextView);
-        nameTextView.setText(currentC.getCurrentCName());
+        viewHolder.nameTextView.setText(currentC.getCurrentCName());
 
         //Set abbrTextView in list_item
-        TextView abbrTextView = (TextView) listItemView.findViewById(R.id.abbrTextView);
-        abbrTextView.setText(currentC.getCurrentCAbbreviations());
+        viewHolder.abbrTextView.setText(currentC.getCurrentCAbbreviations());
 
-        //Set image to flagImageView
-        ImageView flagImageView = (ImageView) listItemView.findViewById(R.id.flagImageView);
-        flagImageView.setImageResource(currentC.getFlagResourcesId());
+        //Set image to flagImageView in list_item
+        viewHolder.flagImageView.setImageResource(currentC.getFlagResourcesId());
 
         return listItemView;
     }
