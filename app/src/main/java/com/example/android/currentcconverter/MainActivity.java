5020e7c84e1e6aee75d07c432fdb01b10760fa3b
Nhan Nguyen <nhan.nguyen@uwaterloo.ca>
2017-10-30 23:27:45 -0400

    Edited Appbar

--- app/src/main/java/com/example/android/currentcconverter/MainActivity.java
+++ app/src/main/java/com/example/android/currentcconverter/MainActivity.java
@@ -14,6 +14,7 @@ import android.text.InputType;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.view.Window;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -53,7 +54,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         //Set up viewpager
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         BasicFragmentPagerAdapter myAdapter = new BasicFragmentPagerAdapter(this, getSupportFragmentManager());
